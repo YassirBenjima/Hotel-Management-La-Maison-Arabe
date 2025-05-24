395b1d5c383f5b206ce925a32796f970eca130f9
Yassir Benjima <Yassirbenjima18@gmail.com>
2025-05-24 11:54:02 +0100

    `Added custom repository methods for Booking, Room and User entities`

--- src/main/java/com/maisonarabe/MaisonArabe/repository/BookingRepository.java
+++ src/main/java/com/maisonarabe/MaisonArabe/repository/BookingRepository.java
@@ -1,2 +1,15 @@
-package com.maisonarabe.MaisonArabe.repository;public interface BookingRepository {
+package com.maisonarabe.MaisonArabe.repository;
+import com.maisonarabe.MaisonArabe.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+
+    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking> findByUserId(Long userId);
+
 }
--- src/main/java/com/maisonarabe/MaisonArabe/repository/UserRepository.java
+++ src/main/java/com/maisonarabe/MaisonArabe/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package com.maisonarabe.MaisonArabe.repository;public class UserRepository {
+package com.maisonarabe.MaisonArabe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.maisonarabe.MaisonArabe.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String email);
+
+    Optional<User> findByEmail(String email);
 }
